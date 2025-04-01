fa73581b0f5aa60fb41f4c9eea83da4507fdaeac
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-04-01 11:35:00 +0530

    🔖 | release v1.1.0 (Dreya)
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 )
 
 var (
-	version      = "v1.0.0 (NOX)"
+	version      = "v1.1.0 (Dreya)"
 	focusedStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#DC143C"))
 	blurredStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#696969"))
 	cursorStyle  = focusedStyle
